3f7f9af367121a96e81bed511b87581e5aee9834
cristianriofrio <cristianriofrio@hotmail.com>
2025-01-31 22:59:30 -0500

    3

--- agenda/src/app/acontacto/acontacto.page.ts
+++ agenda/src/app/acontacto/acontacto.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccesoService } from '../servicio/acceso.service';
 
 @Component({
   selector: 'app-acontacto',
@@ -6,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acontacto.page.scss'],
 })
 export class AcontactoPage implements OnInit {
-
-  constructor() { }
+  contacto: any =[];
+  cod_contacto:string="";
+  cod_persona:string="";
+  txt_nombre:string="";
+  txt_apellido:string="";
+  txt_telefono:string="";
+  txt_correo:string="";
+  constructor(
+    private servicio: AccesoService) 
+    { 
+    this.servicio.getSession("cod_contacto").then((res:any)=>{
+      this.cod_contacto=res;
+      this.cargarDatos();
+    });
+  }
+cargarDatos()
+{
+  let datos={
+    "accion":"dcontacto",
+    "cod_contacto":this.cod_contacto,
+  }
+  this.servicio.postData(datos).subscribe((res:any)=>{
+    if(res.estado)
+    {
+      this.contacto=res.data;
+      this.txt_nombre=this.contacto.nombre;
+      this.txt_nombre=this.contacto.apellido;
+      this.txt_nombre=this.contacto.telefono;
+      this.txt_nombre=this.contacto.correo;
+    }
+    else
+    {
+      this.servicio.showToast(res.mensaje, 3000);
+    }
+  });
+}
 
   ngOnInit() {
   }
